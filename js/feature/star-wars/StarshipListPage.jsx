b4647c99c06856e1075a4eb72b935a45c5cd2ed4
Nathan Wells <nwwells@gmail.com>
2015-09-18 16:48:57 -0600

    adding search by name

--- js/feature/star-wars/StarshipListPage.jsx
+++ js/feature/star-wars/StarshipListPage.jsx
@@ -5,7 +5,7 @@ import DataDrivenComponent from '../../lib/data/DataDrivenComponent';
 import ThemeManager from '../../lib/material/ThemeManager';
 import { TextField } from 'material-ui';
 
-import { sortBy } from 'lodash';
+import { sortBy, includes } from 'lodash';
 
 export default class StarshipListPage extends DataDrivenComponent {
 
@@ -22,23 +22,25 @@ export default class StarshipListPage extends DataDrivenComponent {
 
 	render () {
 		var starships = this.getData('starships')
+		var filter_name = this.getData('filter_name') || "";
 		var less_than = +this.getData('less_than') || 10000000000000;
 		var greater_than = +this.getData('greater_than') || 0;
 		var sort = this.getData('sort') || "name";
 
 		if (starships) {
 			var rows = sortBy(starships, (ship) => {
-					return sort === "cost_in_credits" ? +ship[sort] : ship[sort];
-				})
-				.filter( ship => {
-					return +ship.cost_in_credits > greater_than && +ship.cost_in_credits < less_than;
-				})
-				.map( ship => {
+				return sort === "cost_in_credits" ? +ship[sort] : ship[sort];
+			}).filter( ship => {
+				return ship.name.indexOf(filter_name) !== -1;
+			}).filter( ship => {
+				if (ship.cost_in_credits === "unknown") return true;
+				return +ship.cost_in_credits > greater_than && +ship.cost_in_credits < less_than;
+			}).map( ship => {
 				return <tr key={ship.url}>
 					<td>{ship.name}</td>
 					<td>{ship.cost_in_credits}</td>
 				</tr>
-			})	
+			});
 		} else {
 			var rows = <tr>
 				<td><img src="http://www.arabianbusiness.com/skins/ab.main/gfx/loading_spinner.gif"/></td>
@@ -48,10 +50,13 @@ export default class StarshipListPage extends DataDrivenComponent {
 		return <div>
 
 			<TextField 
-				floatingLabelText="greater than"
+				floatingLabelText="search by name"
+				{ ...this.bindValue('filter_name') } />
+			<TextField 
+				floatingLabelText="costs greater than"
 				{ ...this.bindValue('greater_than') } />
 			<TextField 
-				floatingLabelText="less than"
+				floatingLabelText="costs less than"
 				{ ...this.bindValue('less_than') } />
 
 			<table>
