00dfb7154a118c2eabc6cc7f4adfe67a57757878
Nathan Wells <nwwells@gmail.com>
2015-09-18 15:37:37 -0600

    Remove menu icon

--- js/feature/header/Header.jsx
+++ js/feature/header/Header.jsx
@@ -11,7 +11,7 @@ export default class Header extends React.Component {
 	}
 
 	render () {
-		return <AppBar title="Hello Header!"></AppBar>
+		return <AppBar title="Hello Header!" showMenuIconButton={false}></AppBar>
 	}
 }
 
